9081b01c5bd9e6f763eade61a45b5e07380d83bb
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-12-20 13:13:37 -0600

    Add categories forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root 'splash#index'
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   resources :group_movements
   resources :groups
   resources :movements
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root 'splash#index'
 end
