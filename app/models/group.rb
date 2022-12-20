70945a5acd49c4fb4ec1b612d8e9280b42d4af24
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-12-19 19:27:08 -0600

    Add valitadion on models and add node modules on the gitignore file

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,6 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :group_movements
 
-  validates :name, presence: true
-  validates :icon, presence: true
+  validates :name, presence: true, allow_blank: false
+  validates :icon, presence: true, allow_blank: false
 end
--- app/models/movement.rb
+++ app/models/movement.rb
@@ -2,6 +2,6 @@ class Movement < ApplicationRecord
   belongs_to :user
   has_many :group_movements
 
-  validates :name, presence: true
+  validates :name, presence: true, allow_blank: false
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
