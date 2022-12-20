179d6e3d77bec543ff7634e95c619101d22e4182
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-12-19 19:53:48 -0600

    Add splash page and connect with the group page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :group_movements
   resources :groups
   resources :movements
-  devise_for :users
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'users#index'
+  root 'splash#index'
 end
